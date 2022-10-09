3901f728978d83fcbb3b218c179e4da45cca1d49
Burra <gezerbugra@gmail.com>
2022-10-09 14:17:41 +0300

    virtual database created

---
+++ src/courseAppWithLayeredArchitecture/virtualDatabase/VirtualDatabase.java
@@ -0,0 +1,12 @@
+package courseAppWithLayeredArchitecture.virtualDatabase;
+
+import courseAppWithLayeredArchitecture.entities.Course;
+import courseAppWithLayeredArchitecture.entities.CourseCategory;
+import courseAppWithLayeredArchitecture.entities.Instructor;
+
+class VirtualDatabase {
+
+  public static Course[] courses = new Course[] {};
+  public static CourseCategory[] courseCategories = new CourseCategory[] {};
+  public static Instructor[] instructor = new Instructor[] {};
+}
