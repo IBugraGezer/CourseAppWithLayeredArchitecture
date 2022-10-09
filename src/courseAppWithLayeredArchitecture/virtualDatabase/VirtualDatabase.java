5321b66feac2465f4b94d5a8c96564ff672e7bac
Burra <gezerbugra@gmail.com>
2022-10-09 14:22:08 +0300

    virtual database table types changed to ArrayList

--- src/courseAppWithLayeredArchitecture/virtualDatabase/VirtualDatabase.java
+++ src/courseAppWithLayeredArchitecture/virtualDatabase/VirtualDatabase.java
@@ -3,10 +3,12 @@ package courseAppWithLayeredArchitecture.virtualDatabase;
 import courseAppWithLayeredArchitecture.entities.Course;
 import courseAppWithLayeredArchitecture.entities.CourseCategory;
 import courseAppWithLayeredArchitecture.entities.Instructor;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VirtualDatabase {
 
-  public static Course[] courses = new Course[] {};
-  public static CourseCategory[] courseCategories = new CourseCategory[] {};
-  public static Instructor[] instructor = new Instructor[] {};
+  public static List<Course> courses = new ArrayList<Course>();
+  public static List<CourseCategory> courseCategories = new ArrayList<CourseCategory>();
+  public static List<Instructor> instructor = new ArrayList<Instructor>();
 }
