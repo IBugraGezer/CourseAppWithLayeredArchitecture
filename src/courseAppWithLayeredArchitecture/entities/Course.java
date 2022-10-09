f75b851d5ad998bc58de141964507aea977ca3c9
Burra <gezerbugra@gmail.com>
2022-10-09 15:13:51 +0300

    id fields removed from course and course category entities

--- src/courseAppWithLayeredArchitecture/entities/Course.java
+++ src/courseAppWithLayeredArchitecture/entities/Course.java
@@ -2,17 +2,8 @@ package courseAppWithLayeredArchitecture.entities;
 
 public class Course {
 
-  private int id;
   private String name;
   private double price;
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
 
   public String getName() {
     return name;
