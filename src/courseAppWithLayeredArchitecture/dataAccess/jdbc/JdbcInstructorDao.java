081a4451446ecdefbdb662bdaff5b420d20997fa
Burra <gezerbugra@gmail.com>
2022-10-09 14:36:04 +0300

    JdbcInstructorDao defined

---
+++ src/courseAppWithLayeredArchitecture/dataAccess/jdbc/JdbcInstructorDao.java
@@ -0,0 +1,33 @@
+package courseAppWithLayeredArchitecture.dataAccess.jdbc;
+
+import courseAppWithLayeredArchitecture.dataAccess.IInstructorDao;
+import courseAppWithLayeredArchitecture.entities.Instructor;
+import courseAppWithLayeredArchitecture.virtualDatabase.VirtualDatabase;
+
+public class JdbcInstructorDao implements IInstructorDao {
+
+  public void add(Instructor instructor) {
+    VirtualDatabase.instructors.add(instructor);
+    System.out.println(instructor.getName() + " jdbc ile eklendi");
+  }
+
+  public boolean isExists(String name) {
+    for (Instructor instructor : VirtualDatabase.instructors) {
+      if (instructor.getName() == name) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void remove(Instructor instructorToRemove) {
+    int index = 0;
+    for (Instructor instructor : VirtualDatabase.instructors) {
+      if (instructor.getName() == instructorToRemove.getName()) {
+        VirtualDatabase.instructors.remove(index);
+      }
+      index++;
+    }
+    System.out.println("jdbc ile silindi");
+  }
+}
